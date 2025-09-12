4989e716da8fdc1f9cdf5f55f78a64a1aa263e18
Kacy Do <contact@kacydo.dev>
2025-09-12 10:39:58 +1000

    add layout for InvoicePage

---
+++ app/invoice/[id]/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+	return (
+		<header>
+			<section>Go Back</section>
+			<section>Stuffs...</section>
+		</header>
+	);
+}
---
+++ app/invoice/[id]/page.tsx
@@ -0,0 +1,12 @@
+import Header from './components/Header';
+
+export default function InvoicePage() {
+	return (
+		<>
+			<main>
+				<Header />
+				<section>content</section>
+			</main>
+		</>
+	);
+}
