636a18b2e2e25cced4fbf4669c0ae8d46c384f28
Kacy Do <contact@kacydo.dev>
2025-09-13 11:58:52 +1000

    add supabase client and /example path

---
+++ app/example/page.tsx
@@ -0,0 +1,16 @@
+import { createClient } from '@/utils/supabase/client';
+
+export default async function Invoices() {
+	const supabase = createClient();
+	const { data: instruments } = await supabase.from('invoices_v02').select(
+		// `*, customer:customers_v02 (*, customer_addresses_v02 (*)), invoice_items:invoice_line_items_v02 (*, products_v02 (*, product_prices_v02 (*)))`
+		`
+    status, invoice_id, payment_due,
+    customer: customers_v02 (name), 
+    invoice_items: invoice_line_items_v02 (quantity, products: products_v02 (product_prices: product_prices_v02 (price, currency)))
+    `
+	);
+	// .eq('id', 457)
+	// .single();
+	return <pre>{JSON.stringify(instruments, null, 2)}</pre>;
+}
