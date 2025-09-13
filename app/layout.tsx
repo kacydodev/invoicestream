d1335f0da80f0c10d22df0c642e40fdbddfc7a25
Chinh Do <chinh@doinby.co>
2025-09-14 09:23:33 +1000

    refactor Sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import Navbar from '@/components/Navbar';
+import Sidebar from '@/components/Sidebar';
 import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
@@ -14,9 +14,9 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
+		<html lang='en' className=''>
 			<body>
-				<Navbar />
+				<Sidebar />
 				{children}
 				<Footer />
 			</body>
