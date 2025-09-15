6f87329114458e5dd63839a7c42798b5b890e659
Kacy Do <contact@kacydo.dev>
2025-09-15 11:17:44 +1000

    update tsconfig

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,8 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en' className=''>
+		// <html lang='en' className='dark'>
+		<html lang='en'>
 			<body>
 				<Sidebar />
 				{children}
